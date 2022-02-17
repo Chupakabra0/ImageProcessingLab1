6aee6567bcd51d5eab5535cd56a9adf30b07c4ee
Chupakabra0 <rootalfa4@gmail.com>
2022-02-17 02:04:43 +0200

    Maybe, commit....

--- ImageProcessingLab1/include/MainHeader.hpp
+++ ImageProcessingLab1/include/MainHeader.hpp
@@ -3,9 +3,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
+#include <format>
 #include <algorithm>
-
 using namespace std;
 
 #include "argparse/argparse.hpp"
+
 #include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include "opencv2/core/utils/logger.hpp"
+
--- ImageProcessingLab1/src/Main.cpp
+++ ImageProcessingLab1/src/Main.cpp
@@ -1,6 +1,30 @@
 #include "MainHeader.hpp"
 
 int main(const int argc, char** argv) {
+    cv::utils::logging::setLogLevel(cv::utils::logging::LogLevel::LOG_LEVEL_SILENT);
+
+    auto mat = make_unique<cv::Mat>(cv::imread("img.jpg"));
+
+    cout << std::format("Height: {}\n", mat->rows);
+    cout << std::format("Width: {}\n", mat->cols);
+    cout << std::format("Size: {}\n", mat->rows * mat->cols);
+    cout << std::format("Depth: {}\n", mat->depth());
+    cout << std::format("Channels: {}\n", mat->channels());
+    cout << std::format("Type: {}\n", mat->type());
+
+    cout << std::string(35, '-') << std::endl;
+
+    for (auto row = 0; row < mat->rows; ++row) {
+        for (auto col = mat->cols - 1; col >= mat->cols - 35; --col) {
+            mat->ptr(row, col)[0] = 255;
+            mat->ptr(row, col)[1] = 0;
+            mat->ptr(row, col)[2] = 0;
+        }
+    }
+
+    cv::imshow("TEST!", *mat);
+    cv::waitKey(0);
+    cv::imwrite("result.jpg", *mat);
 
     return EXIT_SUCCESS;
 }
